b4d08a4256bfe58d859b56bba65325eca08ec5ad
David Duncan <davidduncan@Libro-de-David.local>
2024-01-20 09:01:14 -0800

    otravez

--- index.js
+++ index.js
@@ -169,7 +169,16 @@ let buyDrink = isHotOutside === true && moneyInPocket > 10.5;
 console.log("Buy a drink?", buyDrink);
 return buyDrink;
 }
-
 willBuyDrink(true, 11)
+
+/*13*/
+function isPalindrome(word) {
+    for (let i = 0, j = word.length - 1; i < word.length || j > 0; i++, j--) {
+        if (word[i] !== word[j]) return false;
+    }
+return true;
+}
+console.log(isPalindrome("radar"));
+
 
 
